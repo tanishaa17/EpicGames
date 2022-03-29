03c5e85bcab75e3b1fc6560915c4bff765c616bc
Rishabh Gupta <rishabhg.1996@gmail.com>
2022-03-29 23:56:29 +0530

    cart

--- src/Components/Cart/script/cart.jsx
+++ src/Components/Cart/script/cart.jsx
@@ -1,12 +1,35 @@
 import {useState, useEffect} from 'react'
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import "../style/cart.css";
 
 export const GameCart = () => {
 
   return(
-    <div>
-        <p>Hello hi</p>
+    <div id="rcart" className="rgame-cart">
+        <div id="rgamesList"></div>
+        <div id="rgamesPrice">
+            <p className="rGamesandSummary">Games and App Summary</p>
+            <br/>
+            <span id="rshowPrice">
+                <p>Price</p>
+                <p>6184</p>
+            </span>
+            <span id="rshowPrice">
+                <p>Sale Discount</p>
+                <p>-3099</p>
+            </span>
+            <span id="rshowPrice">
+                <p>Taxes</p>
+                <p>NA</p>
+            </span>
+            <div className="rline"></div>
+            <span id="rshowPrice">
+                <p>Subtotal</p>
+                <p>NA</p>
+            </span>
+            <button className="rcheckoutButton">Check Out</button>
+        </div>
     </div>
   );
 };
