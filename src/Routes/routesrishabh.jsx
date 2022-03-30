0e6679dc98699f56cbbec7b5c326c6358e650c6c
Siddalinga <siddaling.gkoudi@gmail.com>
2022-03-30 15:57:29 +0530

    Footer completed

--- src/Routes/routesrishabh.jsx
+++ src/Routes/routesrishabh.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router";
 import { GameCart } from '../Components/Cart/script/cart'
+import {Footer} from "../Components/Footer/script/Footer"
 export const AllRoutes = () => {
   return (
     <>
       <Routes>
         
         <Route exact path="/cart" element={<GameCart />} />
-        
       </Routes>
+      <Footer/>        
     </>
   );
 };
