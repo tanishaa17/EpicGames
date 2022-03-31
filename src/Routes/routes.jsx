6e4ef7b00b2161e731bf660f5c4c5e9c7c054155
tanishaa17 <krati1701@gmail.com>
2022-03-31 08:56:48 +0530

    .

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -5,15 +5,13 @@ import { GameCart } from '../Components/Cart/script/cart';
 import { Footer } from "../Components/Footer/script/Footer"
 import { Login } from "../Components/Login/script/Login";
 import { Signup } from "../Components/Signup/script/Signup";
-<<<<<<< HEAD
 import { IndividualGame } from "../Components/IndividualGame/script/IndividualGame";
 import { SignupOptions } from "../Components/SignupOptions/SignupOptions"
 import { LoginOptions } from '../Components/LoginOptions/LoginOptions';
-=======
 import { SignupOptions } from "../Components/SignupOptions/script/SignupOptions";
 import {IndividualGame} from "../Components/IndividualGame/script/IndividualGame";
 
->>>>>>> 44c65c57ebce97e8fade7671e04ec7bfb9ea03eb
+
 export const AllRoutes = () => {
   return (
     <>
