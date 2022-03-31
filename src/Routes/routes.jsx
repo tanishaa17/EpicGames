159584f5f29b76820de30eba5a2d4836fde19edd
tanishaa17 <krati1701@gmail.com>
2022-03-31 09:00:27 +0530

    .

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -5,8 +5,6 @@ import { GameCart } from '../Components/Cart/script/cart';
 import { Footer } from "../Components/Footer/script/Footer"
 import { Login } from "../Components/Login/script/Login";
 import { Signup } from "../Components/Signup/script/Signup";
-import { IndividualGame } from "../Components/IndividualGame/script/IndividualGame";
-import { SignupOptions } from "../Components/SignupOptions/SignupOptions"
 import { LoginOptions } from '../Components/LoginOptions/LoginOptions';
 import { SignupOptions } from "../Components/SignupOptions/script/SignupOptions";
 import {IndividualGame} from "../Components/IndividualGame/script/IndividualGame";
