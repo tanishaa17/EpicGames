2c37f23ee54ee09ac19b02e97c40a8c7f0745280
tanishaa17 <krati1701@gmail.com>
2022-03-31 14:46:37 +0530

    login and sign up options done

--- src/Components/SignupOptions/script/SignupOptions.jsx
+++ src/Components/SignupOptions/script/SignupOptions.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "../style/SignupOptions.css";
+import { SiEpicgames } from "react-icons/si";
+import { RiFacebookCircleLine } from "react-icons/ri";
+import { FcGoogle } from "react-icons/fc";
+import { IoLogoXbox } from "react-icons/io";
+import { SiPlaystation } from "react-icons/si";
+import { BsApple } from "react-icons/bs";
+import { FaSteam } from "react-icons/fa";
+import { SiNintendonetwork } from "react-icons/si";
 export const SignupOptions = () => {
     return (
         <div className="signupOptionsContainer">
             <div className="optionContainer">
                 <div className="icon">
-                    <img src="\public\icons\Epic_Games_white.svg" alt="logo" />
+                    <SiEpicgames className="logo"></SiEpicgames>
                 </div>
 
                 <p className="heading">
@@ -14,36 +22,36 @@ export const SignupOptions = () => {
 
                 <div className="optionsName">
                     <div className="allowedOption">
-                        <img src="/public/social-icons/Epic_Games.svg" alt="epic-games" />
+                        <SiEpicgames className="epic"></SiEpicgames>
                         <p className="text">SIGN IN WITH EPIC GAMES</p>
                     </div>
                     <div className="option">
-                        <img src="/public/social-icons/Facebook.svg" alt="logo-img" />
+                        <RiFacebookCircleLine className="fb"></RiFacebookCircleLine>
                         <p className="text">SIGN IN WITH FACEBOOK</p>
                     </div>
                     <div className="allowedOption">
-                        <img src="/social-icons/Google.svg" alt="logo-img" />
+                        <FcGoogle className="google"></FcGoogle>
                         <p className="text">SIGN IN WITH GOOGLE</p>
                     </div>
                     <div className="option">
-                        <img src="/social-icons/Nintendo.svg" alt="logo-img" />
-                        <p className="text">SIGN IN WITH NINTENDO ACCOUNT</p>
-                    </div>
-                    <div className="option">
-                        <img src="/social-icons/Xbox.svg" alt="logo-img" />
+                        <IoLogoXbox className="xbox"></IoLogoXbox>
                         <p className="text">SIGN IN WITH XBOX LIVE</p>
                     </div>
                     <div className="option">
-                        <img src="/social-icons/Playstation_Network.svg" alt="logo-img" />
+                        <SiPlaystation className="psn"></SiPlaystation>
                         <p className="text">SIGN IN WITH PLAYSTATION NETWORK</p>
                     </div>
                     <div className="option">
-                        <img src="/social-icons/Apple.svg" alt="logo-img" />
-                        <p className="text">SIGN IN WITH APPLE</p>
+                        <SiNintendonetwork className="ninten"></SiNintendonetwork>
+                        <p className="text">SIGN IN WITH NINTENDO ACCOUNT</p>
                     </div>
                     <div className="option">
-                        <img src="/social-icons/Steam.svg" alt="logo-img" />
+                        <FaSteam className="steam"></FaSteam>
                         <p className="text">SIGN IN WITH STEAM</p>
+                    </div>
+                    <div className="option">
+                        <BsApple className="apple"></BsApple>
+                        <p className="text">SIGN IN WITH APPLE</p>
                     </div>
                 </div>
                 <p className="tfooter">
